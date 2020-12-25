066d86c0cda6853e832bc955e111971b668dbbb0
shenchen.zhao <shenchen.zhao@iherb.com>
2020-12-25 16:28:39 +0800

    Update path to server.js

--- mongoRestApp.js
+++ mongoRestApp.js
@@ -2,10 +2,6 @@
 
 var mongodbRest = require('../../../root/node_modules/mongodb-rest/server.js');
 var config = {
-    "db": "mongodb://m001-student:m001-mongodb-basics@sandbox.sppfg.mongodb.net/sample_airbnb?retryWrites=true&w=majority"
+    "db": "mongodb://m001-student:m001-mongodb-basics@sandbox-shard-00-00.sppfg.mongodb.net:27017,sandbox-shard-00-01.sppfg.mongodb.net:27017,sandbox-shard-00-02.sppfg.mongodb.net:27017/sample_airbnb?ssl=true&replicaSet=atlas-b5vzke-shard-0&authSource=admin&retryWrites=true&w=majority"
 };
 mongodbRest.startServer(config);
-
-
-
-
