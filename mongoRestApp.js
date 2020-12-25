3bd2b63102df376b47698421697404345c532aa8
shenchen.zhao <shenchen.zhao@iherb.com>
2020-12-25 15:41:30 +0800

    Update path to server.js

--- mongoRestApp.js
+++ mongoRestApp.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var mongodbRest = require('mongodb-rest/server.js');
+var mongodbRest = require('../../../root/node_modules/mongodb-rest/server.js');
 var config = {
     "db": "mongodb://m001-student:m001-mongodb-basics@sandbox.sppfg.mongodb.net/sample_airbnb?retryWrites=true&w=majority"
 };
