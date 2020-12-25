cff3e895e4a1fcbfb0a141f422abfca8720c29d9
shenchen.zhao <shenchen.zhao@iherb.com>
2020-12-25 16:16:29 +0800

    Update path to server.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 
-var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require('../../../root/node_modules/mongodb').MongoClient;
 
 var uri = "mongodb://m001-student:m001-mongodb-basics@sandbox-shard-00-00.sppfg.mongodb.net:27017,sandbox-shard-00-01.sppfg.mongodb.net:27017,sandbox-shard-00-02.sppfg.mongodb.net:27017/sample_airbnb?ssl=true&replicaSet=atlas-b5vzke-shard-0&authSource=admin&retryWrites=true&w=majority";
 MongoClient.connect(uri, function(err, client) {
