09bf06f566ce4056677ab0f55ab13a8a42b9fded
shenchen.zhao <shenchen.zhao@iherb.com>
2020-12-28 14:36:12 +0800

    Add patch access to config

--- mongoRestApp.js
+++ mongoRestApp.js
@@ -2,6 +2,11 @@
 
 var mongodbRest = require('../../../root/node_modules/mongodb-rest/server.js');
 var config = {
-    "db": "mongodb://localhost:27017"
+    "db": "mongodb://localhost:27017",
+    "accessControl": {
+        "allowOrigin": "*",
+        "allowMethods": "GET,POST,PUT,PATCH,DELETE,HEAD,OPTIONS",
+        "allowCredentials": false
+    }
 };
 mongodbRest.startServer(config);
